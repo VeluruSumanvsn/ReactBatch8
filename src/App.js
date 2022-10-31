20cbddae3b422022b3acaf56fb38c2e536af6043
Surendra Chithukati <surendra.c@idexcel.com>
2022-10-31 10:51:02 +0530

    new Copmonent Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Home';
 import './App.css';
 import Dashboard from './Components/Dashboard'
 import Header from "./Components/Header";
+import { Navbar } from "./Components/Navbar";
 
 const App = () =>{
   return(
@@ -10,6 +11,7 @@ const App = () =>{
         {/* <Header />
         <Home /> */}
         <Dashboard />
+        <Navbar />
     </div>
    
   )
