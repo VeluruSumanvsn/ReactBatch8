07a50ec5d0b2b8e3589d529f76b5196894b611e4
harishasodi <harishasodi@gmail.com>
2022-11-02 08:36:19 +0530

    title

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Surendra Component</div>
+    <div>Surendra </div>
   )
 }
---
+++ src/Components/title.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const title = () => {
+  return (
+    <div><h1>harish</h1></div>
+  )
+}
