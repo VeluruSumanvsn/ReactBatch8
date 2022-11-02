d83b00a30cc3986d97099a2d039ee0d35e2a162f
VeluruSumanvsn <velurusuman.vsn@gmail.com>
2022-11-02 19:02:16 +0530

    header updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,6 +10,8 @@ const Header = () => {
         <input type="password" placeholder="Password"/>
         <label>Re-Password</label>
         <input type="password" placeholder="Username"/>
+        <label>Email</label>
+        <input type="Email" placeholder="Username"/>
         <input type="button" value="submit" />
       </form>
     </div>
