6513caa56b8711ff79822295b9cb1c7226344883
VeluruSumanvsn <velurusuman.vsn@gmail.com>
2022-11-08 07:38:56 +0530

    form created

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,22 +1,64 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-class Dashboard extends React.Component {
-    state={
-        name: "Kohli",
-        salary: 123456,
-        age:30
+import React, { useState } from "react";
+import "../Styles/AboutUS.css";
+
+function Dashboard() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errormessage, setErrorMessage] = useState({});
+  const dummyData = [
+    {
+      username: "Admin",
+      password: "admin",
+    },
+  ];
+  const errorData = {
+    uname: "Invalid UserName",
+    pass: "Invalid Password"
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    var { uname, pass } = document.forms[0];
+    const userData = dummyData.find((user) => user.username === uname.value);
+    if(userData){
+      if(userData.password !== pass.value){
+        setErrorMessage({name:"pass", message: errorData.pass});
+      }
+      else{
+        setIsSubmitted(true)
+      }
     }
-    render(){
-        const{name, salary, age} = this.state;
-        return(
-            <div>
-               <h1>This is Dashboard</h1>
-               <h1>{name}</h1>
-               <h1>{salary}</h1>
-               <h1>{age}</h1>
-               <Link to='/'>Home</Link>
-            </div>
-        )
+    else{
+      setErrorMessage({name:"uname", message: errorData.uname});
     }
+  };
+  const renderMessage = (name) =>
+    name === errormessage.name && (
+      <div className="error">{errormessage.message}</div>
+    )
+  
+  const renderData = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-item">
+          <label>User Name</label>
+          <input type="text" name="uname" required />
+          {renderMessage("uname")}
+        </div>
+        <div className="input-item">
+          <label>Password</label>
+          <input type="password" name="pass" required />
+          {renderMessage("pass")}
+        </div>
+        <div className="button-item">
+          <input className="button" type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+  return (
+    <div className="app">
+      {isSubmitted ? <div>Successfull Logged In</div> : renderData}
+    </div>
+  );
 }
 export default Dashboard;
